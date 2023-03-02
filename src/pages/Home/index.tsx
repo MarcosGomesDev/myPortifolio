217a7f84fabfa86674bd7814695a5ed647455304
Marcos Gomes <marcosgomesdev@gmail.com>
2023-03-02 16:27:35 -0300

    corrections on Home Component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {} from 'react'
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { Button, ButtonIcon, Container, Description, Highlight, InsideText, MenuSocial, SubTitle, Title } from './styles'
 
-const Home: React.FC = () => {
-    return (
-        <div>Home</div>
-    )
+interface Props {
+    onClick(): void
 }
+
+const Home = React.forwardRef<HTMLElement, Props>(({onClick}, ref) => {
+    return (
+        <Container ref={ref}>
+            <Highlight>Codificando ideias em resultados</Highlight>
+            <SubTitle>Olá, eu sou <InsideText>Marcos</InsideText></SubTitle>
+            <Title>Full-stack Developer</Title>
+            <Description>Explore meu portfólio e veja como minhas linhas de código podem transformar ideias em soluções digitais incríveis.</Description>
+            <Button onClick={onClick}>Saiba mais</Button>
+
+            <MenuSocial>
+                <ButtonIcon>
+                    <FaGithub />
+                </ButtonIcon>
+                <ButtonIcon>
+                    <FaInstagram />
+                </ButtonIcon>
+                <ButtonIcon>
+                    <FaLinkedin />
+                </ButtonIcon>
+            </MenuSocial>
+        </Container>
+    )
+})
 
 export default Home
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+import Image from '../../assets/image.png'
+
+export const Container = styled.section`
+    padding: 30rem 0 11rem 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)), url(${Image});
+    background-size: cover, contain;
+    background-position: center, center 60px;
+    background-repeat: no-repeat, no-repeat;
+`;
+
+export const Title = styled.h2`
+    color: #fff;
+    font-family: 'InterSemiBold';
+    text-align: center;
+    font-size: 75px;
+`;
+
+export const SubTitle = styled.p`
+    color: #fff;
+    font-family: 'PoppinsThin';
+    font-size: 50px;
+    font-weight: 100;
+    text-align: center;
+`;
+
+export const InsideText = styled.span`
+    color: #2ef8a0;
+`;
+
+export const Description = styled.p`
+    color: #fff;
+    font-family: 'PoppinsLight';
+    font-size: 1.4rem;
+    max-width: 45rem;
+    margin: 1rem auto 3rem auto;
+    text-align: center;
+`;
+
+export const Highlight = styled.p`
+    display: inline;
+    font-family: 'PoppinsRegular';
+    background-color: #131313;
+    color: #fff;
+    border-radius: .4rem;
+    padding: .8rem;
+    font-size: 1.4rem;
+    text-transform: uppercase;
+`;
+
+export const Button = styled.button`
+    width: 27rem;
+    height: 4rem;
+    border-radius: .5rem;
+    background-color: #2ef8a0;
+    color: #000;
+    font-weight: 600;
+    cursor: pointer;
+    transition: all .3s;
+
+    :hover {
+        background-color: rgba(46, 248, 160, 0.6);
+        transform: scale(1.1);
+    }
+`;
+
+export const MenuSocial = styled.div`
+    display: flex;
+    margin-top: 10rem;
+    gap: 2rem;
+`;
+
+export const ButtonIcon = styled.a`
+    font-size: 3rem;
+    transition: all .3s;
+    color: #fff;
+    cursor: pointer;
+    border-radius: 2rem;
+    align-items: center;
+    justify-content: center;
+
+    :hover {
+        color: #2ef8a0;
+        transform: scale(1.2);
+    }
+`;
