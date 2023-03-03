e176be008fdd85a26c6f5a6078271129684e3834
Marcos Gomes <marcosgomesdev@gmail.com>
2023-03-03 14:59:34 -0300

    footer component created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FooterContainer, Title } from './styles'
+
+const Footer: React.FC = () => {
+    return (
+        <FooterContainer>
+            <Title>Marcos Gomes &copy; 2023</Title>
+        </FooterContainer>
+    )
+}
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+    background-color: #131313;
+    height: 6rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.h2`
+    font-size: 1.8rem;
+    font-family: 'BarlowRegular';
+    font-weight: 400;
+    color: #fff;
+`;
