c3b634e32b28838857779ed3e1859c31b069a0fc
Marcos Gomes <marcosgomesdev@gmail.com>
2023-03-03 17:31:43 -0300

    section Service created

--- src/pages/Service/index.tsx
+++ src/pages/Service/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
+import { Card, Container, Content, Header, HeaderTitle, Text, Title } from './styles'
 
-const Service = () => {
+const Service = forwardRef<HTMLElement>((props, ref) => {
     return (
-        <div>Service</div>
+        <Container ref={ref}>
+            <Header>
+                <HeaderTitle>Metodologias de trabalho</HeaderTitle>
+                <Text style={{textAlign: 'center'}}>Conheça um pouco de como eu trabalho no dia a dia</Text>
+            </Header>
+            <Content>
+                <Card>
+                    <Title>Desenvolvimento Web</Title>
+                    <Text>
+                    Concentrado no desenvolvimento de sites, páginas únicas, e-commerce e outras aplicações, 
+                    com o objetivo de oferecer uma experiência de alta qualidade e otimização do site excepcional."
+                    </Text>
+                </Card>
+                <Card>
+                    <Title>Desenvolvimento Mobile</Title>
+                    <Text>
+                        Especializado no desenvolvimento mobile utilizando a tecnologia React Native, criando aplicações 
+                        nativas de alta performance, otimizadas para dispositivos móveis, e promovendo uma experiência única para o usuário.
+                    </Text>
+                </Card>
+                <Card>
+                    <Title>Design Responsivo</Title>
+                    <Text>
+                        Meu desenvolvimento é altamente focado em design responsivo, garantindo uma navegação agradável e eficaz em todos os dispositivos.
+                    </Text>
+                </Card>
+                <Card>
+                    <Title>Experiência do usuário</Title>
+                    <Text>
+                        Priorizo a experiência do usuário em nosso desenvolvimento, criando soluções intuitivas e agradáveis.
+                    </Text>
+                </Card>
+            </Content>
+        </Container>
     )
-}
+})
 
 export default Service
--- src/pages/Service/styles.ts
+++ src/pages/Service/styles.ts
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 
-export const Container = styled.section``;
+export const Container = styled.section`
+    max-width: 140rem;
+    margin: 0 auto;
+    padding: 0 2rem;
+    padding-top: 9rem;
+`;
 
-export const Header = styled.div`
-    display: flex;
+export const Header = styled.header`
+    /* display: flex; */
+    width: 100%;
     align-items: center;
     gap: 3rem;
     margin-bottom: 6rem;
+    justify-content: center;
+`;
+
+export const HeaderTitle = styled.h2`
+    font-family: 'PoppinsBold';
+    font-size: 3.4rem;
+    color: #fff;
+    text-align: center;
 `;
 
 export const Content = styled.div`
@@ -14,6 +28,7 @@ export const Content = styled.div`
     flex-wrap: wrap;
     gap: 2rem;
     margin-bottom: 4rem;
+    justify-content: center;
 `;
 
 export const Card = styled.div`
@@ -24,18 +39,24 @@ export const Card = styled.div`
     padding: 2rem;
     border: .2rem solid #131313;
     border-radius: .5rem;
+    transition: all ease .3s;
+
+    :hover {
+        transform: scale(1.1);
+        border-color: #2ef8a0;
+    }
 `;
 
 export const Title = styled.h3`
-    max-width: 45rem;
-    font-family: var(--font-poppins);
-    font-weight: 700;
-    font-size: 3.4rem;
-    color: #fff;
+    color: #ccc;
+    font-size: 2rem;
+    margin-bottom: 2rem;
+    font-family: 'BarlowBold';
 `;
 
 export const Text = styled.p`
-    font-size: 1.9rem;
+    font-size: 1.5rem;
     letter-spacing: .1rem;
     color: #818181;
+    font-family: 'BarlowRegular';
 `;
